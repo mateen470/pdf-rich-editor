6e4785671e68fcdb0a15a195bc9d6848fdddb1db
Abdul Mateen <abdulmateen4890@gmail.com>
2025-03-31 23:04:42 +0200

    add/token-validation-middleware in API-GATEWAY

---
+++ back-end--/src/middleware/tokenValidator.ts
@@ -0,0 +1,39 @@
+//THIS MIDDLEWARE CHECKS FOR THE TOKEN IN THE REQUEST'S HEADER
+//AND VERIFIES THE TOKEN
+
+import { Request, Response, NextFunction } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+interface CustomJwtPayload extends JwtPayload {
+  email: string;
+  name: string;
+}
+
+export default function authMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).send("Access denied. No token provided.");
+  }
+
+  try {
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET as string
+    ) as CustomJwtPayload;
+
+    req.body = {
+      ...req.body,
+      email: decoded.email,
+      name: decoded.name,
+    };
+
+    next();
+  } catch (error: any) {
+    res.status(400).send(error.message || error);
+  }
+}
