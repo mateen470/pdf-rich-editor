003ff8fb573a15db8af109ad34d8baf07de132f7
Abdul Mateen <abdulmateen4890@gmail.com>
2025-04-01 21:59:21 +0200

    add/proxy route auth-service

---
+++ back-end--/src/proxy-routes/auth-proxy-routes.ts
@@ -0,0 +1,19 @@
+import express, { Request } from "express";
+
+// module.exports = router;
+const proxy = require("express-http-proxy");
+
+const router = express.Router();
+
+//PROXIES ARE BEING USED TO REDIRECT THE REQUEST MADE TO API-GATEWAY TOWARDS THE
+//SSO-AUTH SERVICE
+router.use(
+  "/login",
+  proxy(`${process.env.SSO_AUTH_SERVICE_URL}`, {
+    proxyReqPathResolver: (req: Request) => {
+      return "/login";
+    },
+  })
+);
+
+module.exports = router;
