6db8d67a9e1a10fad2235bc6da68f830e63ef89c
Abdul Mateen <abdulmateen4890@gmail.com>
2025-04-02 23:58:44 +0200

    fix/proxy route API-gateway

--- back-end--/src/proxy-routes/auth-proxy-routes.ts
+++ back-end--/src/proxy-routes/auth-proxy-routes.ts
@@ -7,13 +7,13 @@ const router = express.Router();
 
 //PROXIES ARE BEING USED TO REDIRECT THE REQUEST MADE TO API-GATEWAY TOWARDS THE
 //SSO-AUTH SERVICE
-router.use(
-  "/login",
-  proxy(`${process.env.SSO_AUTH_SERVICE_URL}`, {
-    proxyReqPathResolver: (req: Request) => {
-      return "/login";
-    },
-  })
-);
+router.use(fafansafaff
+//   "/login",
+//   proxy(`${process.env.SSO_AUTH_SERVICE_URL}`, {
+//     proxyReqPathResolver: (req: Request) => {
+//       return "/login";
+//     },
+//   })
+// );
 
 module.exports = router;
