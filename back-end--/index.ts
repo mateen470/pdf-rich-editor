23daf503ab4a067482846a35522dddd985dee380
Abdul Mateen <abdulmateen4890@gmail.com>
2025-04-04 22:52:28 +0200

    add/API connection with auth-service

--- back-end--/index.ts
+++ back-end--/index.ts
@@ -2,14 +2,21 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express, { Request, Response } from "express";
+import cors from "cors";
+
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 4000;
+import authRoute from "./src/proxy-routes/auth-proxy-routes";
+app.use("/auth", authRoute);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("PDFaddy API-GATEWAY");
 });
+
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`API-GATEWAY STARTED ON PORT : ${PORT}`);
--- back-end--/src/proxy-routes/auth-proxy-routes.ts
+++ back-end--/src/proxy-routes/auth-proxy-routes.ts
@@ -1,19 +1,17 @@
 import express, { Request } from "express";
-
-// module.exports = router;
-const proxy = require("express-http-proxy");
+import proxy from "express-http-proxy";
 
 const router = express.Router();
 
 //PROXIES ARE BEING USED TO REDIRECT THE REQUEST MADE TO API-GATEWAY TOWARDS THE
 //SSO-AUTH SERVICE
-router.use(fafansafaff
-//   "/login",
-//   proxy(`${process.env.SSO_AUTH_SERVICE_URL}`, {
-//     proxyReqPathResolver: (req: Request) => {
-//       return "/login";
-//     },
-//   })
-// );
+router.use(
+  "/login",
+  proxy(`${process.env.SSO_AUTH_SERVICE_URL}`, {
+    proxyReqPathResolver: (req: Request) => {
+      return "/login";
+    },
+  })
+);
 
-module.exports = router;
+export default router;
