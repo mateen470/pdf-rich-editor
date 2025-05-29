46ebc278d3bd499fb057008a54d46f2698aaf93e
Abdul Mateen <abdulmateen4890@gmail.com>
2025-05-30 00:06:26 +0200

    add/accordin in dashboard

--- front-end--/src/pages/editor/Dashboard.tsx
+++ front-end--/src/pages/editor/Dashboard.tsx
@@ -4,6 +4,12 @@ import {
     SidebarProvider,
     SidebarTrigger,
 } from "@/components/ui/sidebar"
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/ui/accordion"
 
 import { AppSidebar } from "../../components/editor-components/AppSidebar"
 import { HistoryTable } from "@/components/editor-components/HistoryTable"
@@ -17,18 +23,40 @@ export default function Dashboard() {
                     <div className="flex items-center gap-2 px-4">
                         <SidebarTrigger className="-ml-1" />
                         <Separator orientation="vertical" className="mr-2 h-4" />
-           
+
                     </div>
                 </header>
                 <div className="flex flex-col gap-4 p-4 h-[calc(100vh-64px)]">
-             
+
                     <div className="flex gap-4 h-[20%]">
 
                         <div className=" rounded-xl bg-muted/50 p-10 flex-1">
-                        <HistoryTable />
+                            <HistoryTable />
                         </div>
+                        <Accordion type="single" collapsible className="w-full">
+                            <AccordionItem value="item-1">
+                                <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                                <AccordionContent>
+                                    Yes. It adheres to the WAI-ARIA design pattern.
+                                </AccordionContent>
+                            </AccordionItem>
+                            <AccordionItem value="item-2">
+                                <AccordionTrigger>Is it styled?</AccordionTrigger>
+                                <AccordionContent>
+                                    Yes. It comes with default styles that matches the other
+                                    components&apos; aesthetic.
+                                </AccordionContent>
+                            </AccordionItem>
+                            <AccordionItem value="item-3">
+                                <AccordionTrigger>Is it animated?</AccordionTrigger>
+                                <AccordionContent>
+                                    Yes. It&apos;s animated by default, but you can disable it if you
+                                    prefer.
+                                </AccordionContent>
+                            </AccordionItem>
+                        </Accordion>
                         <div className=" rounded-xl bg-muted/50 p-10 flex-1">
-                        <HistoryTable />
+                            <HistoryTable />
                         </div>
                     </div>
                 </div>
