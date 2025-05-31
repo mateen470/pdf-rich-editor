09e923ab0e593a7c079e1e20505e313cf329bf1e
Abdul Mateen <abdulmateen4890@gmail.com>
2025-05-31 19:17:02 +0200

    update/dashboard

--- front-end--/src/pages/editor/Dashboard.tsx
+++ front-end--/src/pages/editor/Dashboard.tsx
@@ -4,12 +4,7 @@ import {
     SidebarProvider,
     SidebarTrigger,
 } from "@/components/ui/sidebar"
-import {
-    Accordion,
-    AccordionContent,
-    AccordionItem,
-    AccordionTrigger,
-} from "@/components/ui/accordion"
+
 
 import { AppSidebar } from "../../components/editor-components/AppSidebar"
 import { HistoryTable } from "@/components/editor-components/HistoryTable"
@@ -33,28 +28,6 @@ export default function Dashboard() {
                         <div className=" rounded-xl bg-muted/50 p-10 flex-1">
                             <HistoryTable />
                         </div>
-                        <Accordion type="single" collapsible className="w-full">
-                            <AccordionItem value="item-1">
-                                <AccordionTrigger>Is it accessible?</AccordionTrigger>
-                                <AccordionContent>
-                                    Yes. It adheres to the WAI-ARIA design pattern.
-                                </AccordionContent>
-                            </AccordionItem>
-                            <AccordionItem value="item-2">
-                                <AccordionTrigger>Is it styled?</AccordionTrigger>
-                                <AccordionContent>
-                                    Yes. It comes with default styles that matches the other
-                                    components&apos; aesthetic.
-                                </AccordionContent>
-                            </AccordionItem>
-                            <AccordionItem value="item-3">
-                                <AccordionTrigger>Is it animated?</AccordionTrigger>
-                                <AccordionContent>
-                                    Yes. It&apos;s animated by default, but you can disable it if you
-                                    prefer.
-                                </AccordionContent>
-                            </AccordionItem>
-                        </Accordion>
                         <div className=" rounded-xl bg-muted/50 p-10 flex-1">
                             <HistoryTable />
                         </div>
