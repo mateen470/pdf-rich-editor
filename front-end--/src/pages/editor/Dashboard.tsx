b2084fa73a920b779f522a3953de0568fbe270e2
Abdul Mateen <abdulmateen4890@gmail.com>
2025-06-05 23:32:05 +0200

    update/dashboard

--- front-end--/src/pages/editor/Dashboard.tsx
+++ front-end--/src/pages/editor/Dashboard.tsx
@@ -11,30 +11,7 @@ import { HistoryTable } from "@/components/editor-components/HistoryTable"
 
 export default function Dashboard() {
     return (
-        <SidebarProvider>
-            <AppSidebar />
-            <SidebarInset>
-                <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-                    <div className="flex items-center gap-2 px-4">
-                        <SidebarTrigger className="-ml-1" />
-                        <Separator orientation="vertical" className="mr-2 h-4" />
 
-                    </div>
-                </header>
-                <div className="flex flex-col gap-4 p-4 h-[calc(100vh-64px)]">
-
-                    <div className="flex gap-4 h-[20%]">
-                        <div className=" rounded-xl bg-muted/50 p-10 flex-1">
                             <HistoryTable />
-                        </div>
-                    </div>
-                    <div className="flex gap-4 h-[20%]">
-                        <div className=" rounded-xl bg-muted/50 p-10 flex-1">
-                            <HistoryTable />
-                        </div>
-                    </div>
-                </div>
-            </SidebarInset>
-        </SidebarProvider>
     )
 }
