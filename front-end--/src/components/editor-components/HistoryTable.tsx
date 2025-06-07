6af3b7bd4f7cccdaa747efaf1152b0ad28aeaf9e
Abdul Mateen <abdulmateen4890@gmail.com>
2025-06-07 23:52:25 +0200

    update/dashboard

--- front-end--/src/components/editor-components/HistoryTable.tsx
+++ front-end--/src/components/editor-components/HistoryTable.tsx
@@ -67,7 +67,6 @@ export function HistoryTable() {
         <TableHeader>
           <TableRow>
             <TableHead className="w-[100px]">Invoice</TableHead>
-            <TableHead>Status</TableHead>
             <TableHead>Method</TableHead>
             <TableHead className="text-right">Amount</TableHead>
           </TableRow>
