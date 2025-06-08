776a4c2e361a7e67bdf3a6f98dcf01016e5b6b39
Abdul Mateen <abdulmateen4890@gmail.com>
2025-06-08 13:48:28 +0200

    update/front-end cleanup

--- front-end--/src/App.tsx
+++ front-end--/src/App.tsx
@@ -6,9 +6,6 @@ import Register from "./pages/auth/Register"
 import ForgotPassword from "./pages/auth/ForgotPassword"
 import ResetPassword from "./pages/auth/ResetPassword"
 
-//editor
-import Dashboard from "./pages/editor/Dashboard"
-
 function App() {
 
   return (
@@ -18,9 +15,6 @@ function App() {
       <Route path="/register" element={<Register />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
       <Route path="/reset-password" element={<ResetPassword />} />
-
-      {/* editor */}
-      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   )
 }
--- front-end--/src/pages/editor/Dashboard.tsx
+++
@@ -1,7 +0,0 @@
-import { HistoryTable } from "@/components/editor-components/HistoryTable"
-
-export default function Dashboard() {
-    return (
-        <HistoryTable />
-    )
-}
